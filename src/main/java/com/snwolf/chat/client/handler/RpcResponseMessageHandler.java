001eb6bfa0aaef80e89ea5278e06096979e63073
SnowWolf68 <zhangjiashuo0818@163.com>
2024-07-08 00:22:35 +0800

    fix: 1) 及时删除map中用完的Promise,  2) RpcResponseMessageHandler标记为Sharable

--- src/main/java/com/snwolf/chat/client/handler/RpcResponseMessageHandler.java
+++ src/main/java/com/snwolf/chat/client/handler/RpcResponseMessageHandler.java
@@ -2,6 +2,7 @@ package com.snwolf.chat.client.handler;
 
 import cn.hutool.core.util.ObjectUtil;
 import com.snwolf.chat.message.RpcResponseMessage;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.util.concurrent.Promise;
@@ -17,13 +18,14 @@ import java.util.concurrent.ConcurrentHashMap;
  * @Description:
  */
 @Slf4j
+@ChannelHandler.Sharable
 public class RpcResponseMessageHandler extends SimpleChannelInboundHandler<RpcResponseMessage> {
 
     public static final Map<Integer, Promise<Object>> PROMISES = new ConcurrentHashMap<>();
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, RpcResponseMessage msg) throws Exception {
-        Promise<Object> promise = PROMISES.get(msg.getSequenceId());
+        Promise<Object> promise = PROMISES.remove(msg.getSequenceId());
         if (ObjectUtil.isNotNull(promise)) {
             if (ObjectUtil.isNull(msg.getExceptionValue())) {
                 promise.setSuccess(msg.getReturnValue());
